eadcfda63812b201dfcdb41778d624ecb8666ba6
Lisa Millet <lisamilletpro@gmail.com>
2023-03-07 12:57:02 +0100

    test push

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :journey, only: %i[show]
+  resources :user_journeys, only: %i[show create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

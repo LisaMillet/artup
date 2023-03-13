b1b75b85ef53c7c88523d0843bd27292ef718c0a
Micael De Miranda <demirandamicael@gmail.com>
2023-03-13 17:33:25 +0100

    added red and green answers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,14 @@ application.register("hello", HelloController)
 
 import ScannerController from "./scanner_controller"
 application.register("scanner", ScannerController)
+
+import QuestionController from "./question_controller"
+application.register("question", QuestionController)
+// Stimulus install
+// import { Application } from "@hotwired/stimulus"
+// import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+// window.Stimulus = Application.start()
+// const context = require.context("./controllers", true, /\.js$/)
+// Stimulus.load(definitionsFromContext(context))
+// end of Stimulus install
